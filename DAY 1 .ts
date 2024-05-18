e1bee84d10d0be980a85b4a1fbc78cd4c0f869b0
umarkhan <ku6205939@gmail.com>
2024-05-18 19:31:20 +0500

    updates

---
+++ DAY 1 .ts
@@ -0,0 +1,17 @@
+// task 1
+// installation of applications and set up pc 
+
+// task 2
+let personName: string="UMAR KHAN";
+console.log(`hello ${personName} would you like to learn some pyton today?`);
+
+// task 3
+//  lower case
+let personname:string="Umar"
+console.log("lowercase:",personname.toLowerCase());
+
+// uper case
+console.log("upercase:",personname.toLocaleUpperCase());
+
+// title case
+console.log("titlecase:",personname.replace(/\bw/g,c => c.toUpperCase()));
