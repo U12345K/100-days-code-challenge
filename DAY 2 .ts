dbc230a8f3e4bb4100bd7ded59f4fb5a06615716
umarkhan <ku6205939@gmail.com>
2024-05-18 19:34:32 +0500

    updates

---
+++ DAY 2 .ts
@@ -0,0 +1,16 @@
+// task 1
+let quote: string= "a person who never made a mistake never tried anything new";
+let auther: string= "Albert Einstein"
+console.log(`${auther} once said,"${quote}"`);
+
+// task 2
+let qote: string= "a person who never made a mistake never tried anything new";
+let famous_person: string= "Albert Einstein";
+let message=`${famous_person} ones said, "${qote}"`;
+console.log(message);
+
+// task 3
+let persoNname: string = `\n\t IMRAN KHAN\t\n`;
+console.log(persoNname);
+let stripped: string = persoNname.trim();
+console.log(stripped);
